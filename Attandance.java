f6b3b40eebf87dc3ebf64698db046a5e42429487
akinator001 <aakashgandhi1279@gmail.com>
2020-09-22 22:47:57 +0530

    shubh

--- Attandance.java
+++ Attandance.java
@@ -4,6 +4,16 @@ public class Attandance {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main Branch");	
+		
+		int IS_FULL_TIME = 1 ;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Is Present");
+		}
+		else {
+			System.out.println("Not present");
+		}
+		
 	}
-
 }
