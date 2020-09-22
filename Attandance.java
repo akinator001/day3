b2382e05413415bbe5c9c6963e2b63c89c8c3507
akinator001 <aakashgandhi1279@gmail.com>
2020-09-23 00:12:39 +0530

    uc3 completed...

--- Attandance.java
+++ Attandance.java
@@ -6,14 +6,19 @@ public class Attandance {
 			
 		
 		int IS_FULL_TIME = 1 ;
+		int IS_PART_TIME = 2 ;
+		
 		int EMP_RATE = 20 ;
 		int empHrs = 0 ;
 		int empWages = 0 ;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
 		if(empCheck == IS_FULL_TIME) {
 			empHrs = 8 ; 
+		}
+		else if(empCheck == IS_PART_TIME){
+			empHrs = 4;
 		}
 		else {
 			empHrs = 0 ;
